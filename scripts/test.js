f1cd1441cc0e49278fcc04f5c6ea60f0f2e31572
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-05-19 21:51:36 -0700

    yayuh

--- scripts/test.js
+++ scripts/test.js
@@ -3,11 +3,6 @@ module.exports = function(robot) {
        return res.send("http://i.imgur.com/1ZbCPqz.jpg");
      });
     robot.respond(/practice/i, function(res){
-     var spells = ["*felo'mena'SHA!*", "*felo'mena'SHAW!*", "_felo... mena... SHA!_", "*malore!*", "_felomenashaaa!_"];
-     return res.send(res.random(spells));
-	});
+    	var spells = ["*hello*", "*i love you*"]
+    	return res.send(res.random(spells));
 };
-
-
-
-
