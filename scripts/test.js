df84f90acefb970c92e8fd5d0df306b72d90e518
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-05-19 21:22:56 -0700

    hey

--- scripts/test.js
+++ scripts/test.js
@@ -4,7 +4,6 @@ module.exports = function(robot) {
      });
     robot.respond(/is it a (weekend|holiday)\s?\?/i, function(msg){
         var today = new Date();
-
         msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
     });
 };
